a80136ea927ad1946b4c8b0c7af532169aa35f69
sexychoco <gaeun819@naver.com>
2022-07-21 16:18:48 +0900

    plotly

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
+from data import countries_df 
+from builders import make_table
+
+print(countries_df)
 
 stylesheets = [
     "https://cdn.jsdelivr.net/npm/reset-css@5.0.1/reset.min.css",
@@ -10,19 +14,44 @@ stylesheets = [
 
 app = dash.Dash(__name__, external_stylesheets=stylesheets)
 
+bubble_map = px.scatter_geo(
+    countries_df,
+    size="Confirmed",
+    hover_name="Country_Region",
+    color="Confirmed",
+    locations="Country_Region",
+    locationmode="country names",
+    size_max=40,
+    template="plotly_dark",
+    projection="natural earth",
+    hover_data={
+        "Confirmed": ":,2f",
+        "Deaths": ":,2f",
+        "Recovered": ":,2f",
+        "Country_Region": False,
+    },
+)
+
+
 app.layout = html.Div(
-    style = {
+    style={
         "minHeight": "100vh",
         "backgroundColor": "#111111",
-        "color":"white",
-        "fontFamily" : "Open Sans,sans-serif",
+        "color": "white",
+        "fontFamily": "Open Sans, sans-serif",
     },
-    children = [
+    children=[
         html.Header(
-            style={"textAlign":"center", "paddingTop":"50px"},
-            children = [html.H1("Corona Dashboard", style={"fontSize": 40})]
-        )
-    ]
+            style={"textAlign": "center", "paddingTop": "50px", "marginBottom": 100},
+            children=[html.H1("Corona Dashboard", style={"fontSize": 40})],
+        ),
+        html.Div(
+            children=[
+                html.Div(children=[dcc.Graph(figure=bubble_map)]),
+                html.Div(children=[make_table(countries_df)]),
+            ]
+        ),
+    ],
 )
 
 
